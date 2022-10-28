6d4fb4c2a8695882443133d35007d5b0c6a13beb
LizandroV <lizandrovivanco16@gmail.com>
2022-10-28 17:17:04 -0500

    Se esta volviendo a actualizar

--- Forti_Update3.0.py
+++ Forti_Update3.0.py
@@ -1,12 +1,15 @@
+from glob import glob
 import os
 from tkinter import messagebox
 from win32api import *
 import subprocess
+from getpass import getuser
 
 FortiClientx64 = os.path.isfile("C:\Program Files\Fortinet\FortiClient\FortiClient.exe")
 FortiClientx86 = os.path.isfile("C:\Program Files (x86)\Fortinet\FortiClient\FortiClient.exe")
 BackupVPN = os.path.isfile("C:\\TSC\\VPN\\MiVPN.conf")
 file_pathx64 = r'C:\Program Files\Fortinet\FortiClient\FortiClient.exe'
+CREATE_NO_WINDOW = 0x08000000
 
 def get_version_numberx64(file_pathx64):
     if FortiClientx64 == True:
@@ -15,11 +18,10 @@ def get_version_numberx64(file_pathx64):
         return [str(HIWORD(ms_file_version))]
 
 def uninstallVPN():
+    global CREATE_NO_WINDOW
     try:
-        CREATE_NO_WINDOW = 0x08000000
         subprocess.call("""wmic product where "name like 'Forti%%'" call uninstall /nointeractive""", creationflags=CREATE_NO_WINDOW)
     except:
-        CREATE_NO_WINDOW = 0x08000000
         subprocess.call("""echo product where "name like 'Forti%%' " call uninstall /nointeractive|wmic && shutdown /a""", creationflags=CREATE_NO_WINDOW)
 
 if __name__ == "__main__":
@@ -28,38 +30,39 @@ if __name__ == "__main__":
         if FortiClientx64 == True:
             versionx64 = int(".".join(get_version_numberx64(file_pathx64)))
             if versionx64 < 7:
-                messagebox.showwarning(message="Su equipo se reiniciará para actualizar el Software, Presione aceptar para continuar", title="FortiVPN")
+                messagebox.showwarning(message="Su equipo se debe reiniciar para actualizar el Software VPN, Presione aceptar para reiniciar ahora", title="¡ALERTA!")
                 os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
                 os.system('"FCConfig -m all -f "C:\\TSC\\VPN\\MiVPN.conf" -o export -i 1 -p Nuevo.123"')
                 uninstallVPN()
 
         elif FortiClientx86 == True:
-            messagebox.showwarning(message="Su equipo se reiniciará para actualizar el Software, Presione aceptar para continuar", title="FortiVPN")
+            messagebox.showwarning(message="Su equipo se debe reiniciar para actualizar el Software VPN, Presione aceptar para reiniciar ahora", title="¡ALERTA!")
             os.chdir("C:\\Program Files (x86)\\Fortinet\\FortiClient")
             os.system('"FCConfig -m all -f "C:\\TSC\\VPN\\MiVPN.conf" -o export -i 1 -p Nuevo.123"')
             uninstallVPN()
 
         ###INSTALL NEW FORTICLIENT###
         if FortiClientx64 == False:
-            CREATE_NO_WINDOW = 0x08000000
-
-             
             subprocess.call('C:\TSC\VPN\FortiClientVPN.exe /quiet /norestart', creationflags=CREATE_NO_WINDOW)
 
             ###RESTORE CONFIG###
             os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
             if BackupVPN == True:
                 os.system('"FCConfig -m all -f "C:\TSC\VPN\MiVPN.conf" -o import -i 1 -p Nuevo.123"')
+                os.system('"FCConfig -m all -f "C:\TSC\VPN\Lima_VPN.conf" -o import -i 1 -p Nuevo.123"')
             else:
-                os.system('"FCConfig -m all -f "C:\TSC\VPN\VPN.conf" -o import -i 1 -p Nuevo.123"')
+                os.system('"FCConfig -m all -f "C:\TSC\VPN\Lima_VPN.conf" -o import -i 1 -p Nuevo.123"')
 
             os.system("taskkill /f /im FortiClient.exe")
             os.system("taskkill /f /im FortiTray.exe")
+            os.remove("""C://Users//Public//Desktop//FortiClient VPN.lnk""")
 
-            messagebox.showinfo(message="Actualización completada", title="FortiVPN")
-
+            messagebox.showinfo(message="Actualización completada", title="Forticlient_VPN")
+            messagebox.showinfo(message="Tiene instalada la última versión del FortiClient,\nIngrese con el usuario: " + str(getuser()) +" y contraseña de su laptop", title="Forticlient_VPN")
         else:
-            messagebox.showinfo(message="Tiene instalada la última versión del FortiClient", title="FortiVPN")
+            os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
+            os.system('"FCConfig -m all -f "C:\TSC\VPN\Lima_VPN.conf" -o import -i 1 -p Nuevo.123"')
+            messagebox.showinfo(message="Tiene instalada la última versión del FortiClient,\nIngrese con el usuario: " + str(getuser()) +" y contraseña de su laptop", title="Forticlient_VPN")
 
     except:
-        messagebox.showerror(message="Error de actualización", title="FortiVPN")
+        messagebox.showerror(message="Error de actualización", title="Forticlient_VPN")
--- Test1.py
+++ Test1.py
@@ -1,16 +1,62 @@
+from glob import glob
 import os
-from tkinter import messagebox
 from win32api import *
+import subprocess
+from getpass import getuser
 
 FortiClientx64 = os.path.isfile("C:\Program Files\Fortinet\FortiClient\FortiClient.exe")
 FortiClientx86 = os.path.isfile("C:\Program Files (x86)\Fortinet\FortiClient\FortiClient.exe")
 BackupVPN = os.path.isfile("C:\\TSC\\VPN\\MiVPN.conf")
 file_pathx64 = r'C:\Program Files\Fortinet\FortiClient\FortiClient.exe'
-os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
+CREATE_NO_WINDOW = 0x08000000
 
-if BackupVPN == True:
-    print("1")
-    os.system('"FCConfig -m all -f "C:\TSC\VPN\MiVPN.conf" -o import -i 1 -p Nuevo.123"')
-else:
-    print("2")
-    os.system('"FCConfig -m all -f "C:\TSC\VPN\VPN.conf" -o import -i 1 -p Nuevo.123"')
+def get_version_numberx64(file_pathx64):
+    if FortiClientx64 == True:
+        File_information = GetFileVersionInfo(file_pathx64, "\\")
+        ms_file_version = File_information['FileVersionMS']
+        return [str(HIWORD(ms_file_version))]
+
+def uninstallVPN():
+    global CREATE_NO_WINDOW
+    try:
+        subprocess.call("""wmic product where "name like 'Forti%%'" call uninstall /nointeractive""", creationflags=CREATE_NO_WINDOW)
+    except:
+        subprocess.call("""echo product where "name like 'Forti%%' " call uninstall /nointeractive|wmic && shutdown /a""", creationflags=CREATE_NO_WINDOW)
+
+if __name__ == "__main__":
+    try:
+        ###GENERATE BACKUP AND UNISTALL FORTICLIENT###
+        if FortiClientx64 == True:
+            versionx64 = int(".".join(get_version_numberx64(file_pathx64)))
+            if versionx64 < 7:
+                os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
+                os.system('"FCConfig -m all -f "C:\\TSC\\VPN\\MiVPN.conf" -o export -i 1 -p Nuevo.123"')
+                uninstallVPN()
+
+        elif FortiClientx86 == True:
+            os.chdir("C:\\Program Files (x86)\\Fortinet\\FortiClient")
+            os.system('"FCConfig -m all -f "C:\\TSC\\VPN\\MiVPN.conf" -o export -i 1 -p Nuevo.123"')
+            uninstallVPN()
+
+        ###INSTALL NEW FORTICLIENT###
+        if FortiClientx64 == False:
+            subprocess.call('C:\TSC\VPN\FortiClientVPN.exe /quiet /norestart', creationflags=CREATE_NO_WINDOW)
+
+            ###RESTORE CONFIG###
+            os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
+            if BackupVPN == True:
+                os.system('"FCConfig -m all -f "C:\TSC\VPN\MiVPN.conf" -o import -i 1 -p Nuevo.123"')
+                os.system('"FCConfig -m all -f "C:\TSC\VPN\Lima_VPN.conf" -o import -i 1 -p Nuevo.123"')
+            else:
+                os.system('"FCConfig -m all -f "C:\TSC\VPN\Lima_VPN.conf" -o import -i 1 -p Nuevo.123"')
+
+            os.system("taskkill /f /im FortiClient.exe")
+            os.system("taskkill /f /im FortiTray.exe")
+            os.remove("""C://Users//Public//Desktop//FortiClient VPN.lnk""")
+
+        else:
+            os.chdir("C:\\Program Files\\Fortinet\\FortiClient")
+            os.system('"FCConfig -m all -f "C:\TSC\VPN\Lima_VPN.conf" -o import -i 1 -p Nuevo.123"')
+            
+    except:
+        pass
